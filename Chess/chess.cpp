179acb750469ed3edfc41db804b95b396996afb9
ALeliuhin <96695302+ALeliuhin@users.noreply.github.com>
2024-06-04 13:22:53 +0300

    Add files via upload

--- Chess/chess.cpp
+++ Chess/chess.cpp
@@ -1003,7 +1003,7 @@ bool Board::makeMove(int x1, int y1, int x2, int y2) {
 	using namespace std;
 	if (x1 < 0 || x1>7 || y1 < 0 || y1>7 || x2 < 0 || x2>7 || y2 < 0 || y2>8)
 	{
-		std::cout << "One of your inputs was out of bounds" << std::endl;
+		std::cout << BOLD_RED << "One of your inputs was out of bounds" << RESET << std::endl;
 		return false;
 	}
 	Square* src = getSquare(x1, y1);
@@ -1011,7 +1011,7 @@ bool Board::makeMove(int x1, int y1, int x2, int y2) {
 
 	if (src->getColor() == dest->getColor() && dest->getColor() != NONE)
 	{
-		std::cout << "Invalid move: cannot land on your own piece" << std::endl;
+		std::cout << BOLD_RED << "Invalid move: cannot land on your own piece" << RESET << std::endl;
 		return false;
 	}
 
@@ -1029,7 +1029,7 @@ bool Board::makeMove(int x1, int y1, int x2, int y2) {
 		break;
 	case PAWN: return movePawn(src, dest);
 		break;
-	case EMPTY: std::cout << "You do not have a piece there" << std::endl;  return false;
+	case EMPTY: std::cout << BOLD_RED << "You do not have a piece there" << RESET << std::endl;  return false;
 		break;
 	default: std::cerr << "Something went wrong in the switch statement in makeMove()" << std::endl;
 		break;
